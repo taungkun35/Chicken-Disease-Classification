aabd91deaa623d6cf845c7ed6da22f90c3eaee59
Taungkun Thumsattaya <taungkun35@gmail.com>
2023-06-19 23:54:12 +0700

    Prepare base model completed

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTraningPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = DataIngestionTraningPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = PrepareBaseModelTraningPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx======x")
 except Exception as e:
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,8 +1,10 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml,create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig)
+                                                
 
-class ConfiguationManager:
+class ConfigurationManager:
     def __init__(self,
                  config_filepath=CONFIG_FILE_PATH,
                  params_filepath=PARAMS_FILE_PATH):
@@ -22,3 +24,20 @@ class ConfiguationManager:
             unzip_dir = config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        create_directories([config.root_dir])
+        
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=config.root_dir,
+            base_model_path=config.base_model_path,
+            updated_base_model_path=config.updated_base_model_path,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+        
+        return prepare_base_model_config
